95e9be7e04e945b55cca974ee1ea0969a33ea797
Gaurav Lodha <gauravlodha77@gmail.com>
2017-06-22 17:09:19 +0530

    Initial commit

---
+++ MVC_2/Controllers/EmployeeController.cs
@@ -0,0 +1,27 @@
+﻿using MVC_2.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace MVC_2.Controllers
+{
+    public class EmployeeController : Controller
+    {
+        // GET: Employee
+        public ActionResult Details(int id=1)
+        {
+            EmplyeeContext ec = new EmplyeeContext();
+            Employee e = ec.Employees.Single(x => x.Id == id);
+            return View(e);
+        }
+
+        public ActionResult Index()
+        {
+            EmplyeeContext empc = new EmplyeeContext();
+            List<Employee> emp = empc.Employees.ToList();
+            return View(emp);
+        }
+    }
+}
---
+++ MVC_2/Models/Employee.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations.Schema;
+using System.Linq;
+using System.Web;
+
+namespace MVC_2.Models
+{
+    [Table("Employees")]
+    public class Employee
+    {
+        [Column("Id")]
+        public int Id { set; get; }
+        public String Name { set; get; }
+        public String Gender { set; get; }
+        public String City { set; get; }
+    }
+}
